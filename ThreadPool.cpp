ab12d7727df5e4b98e93cffd6369aebb75c40a4a
Artem Karpov <artem.ka1001pm@gmail.com>
2021-08-01 13:18:31 +0300

    made header and source for the ThreadPool class

---
+++ ThreadPool.cpp
@@ -0,0 +1,46 @@
+#include "ThreadPool.h"
+
+ThreadPool::ThreadPool(unsigned int numberThreads)  : _isStop(false)
+{
+    for (unsigned i = 0; i < numberThreads; ++i)
+    {
+        this->_workers.emplace_back([&]()
+        {
+            std::function < void() > task;
+
+            while (!_isStop)
+            {
+                {
+                    std::unique_lock<std::mutex> lock(_tasksMutex);
+
+                    if (_tasks.empty())
+                    {
+                        _waiterForNewTask.wait(lock);
+
+                        if (_isStop || _tasks.empty())
+                        {
+                            break;
+                        }
+                    }
+
+                    task = _tasks.front();
+                    _tasks.pop();
+                }
+
+                task();
+            }
+        });
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+    _isStop = true;
+
+    _waiterForNewTask.notify_all();
+
+    for (auto &worker : _workers)
+    {
+        worker.join();
+    }
+}
---
+++ ThreadPool.h
@@ -0,0 +1,48 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <vector>
+#include <queue>
+
+#include <thread>
+#include <mutex>
+#include <functional>
+#include <future>
+
+class ThreadPool {
+public:
+    explicit ThreadPool(unsigned numberThreads = std::thread::hardware_concurrency());
+
+    template<typename F, typename ...Args>
+    auto enqueue(F &&taskFunction, Args &&...args) -> std::future<typename std::invoke_result<F, Args ...>::type>
+    {
+        using returnType = typename std::invoke_result<F, Args ...>::type;
+
+        auto task = std::make_shared<std::packaged_task<returnType()>>([&]() { return taskFunction(args...); });
+        auto future = (*task).get_future();
+
+        {
+            std::unique_lock<std::mutex> lock(_tasksMutex);
+
+            _tasks.push([=]() { (*task)(); });
+        }
+
+        _waiterForNewTask.notify_one();
+
+        return future;
+    }
+
+    ~ThreadPool();
+
+private:
+    std::vector<std::thread> _workers;
+
+    std::queue<std::function<void()>> _tasks;
+    std::mutex _tasksMutex;
+
+    std::condition_variable _waiterForNewTask;
+
+    bool _isStop;
+};
+
+#endif //THREADPOOL_H
